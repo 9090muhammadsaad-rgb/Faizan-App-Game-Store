91010c60ff9dfd3cd80d52f0ba1e4a34aca57cff
Saad <9090muhammadsaad@gmail.com>
2026-01-05 02:09:37 +0500

    Create game.fs

---
+++ game.fs
@@ -0,0 +1,11 @@
+//!/bin/faizanstore
+// faizanstore programming languge
+// Simple Game
+Choose = .INPUT("Enter Number Start 1-9")endl;
+check = random(1 , 9);
+
+if choose == check:
+   show {"You Are Win The Game"}
+else:
+     show {"Your are Loss The Game Because Number is ", $(check)}
+done
